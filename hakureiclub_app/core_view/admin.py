888972d73e5ba21a2cf92f1f23fa48db068b4349
m85091081 <m85091081@gmail.com>
2017-01-09 14:59:30 +0800

    fix admin

--- hakureiclub_app/core_view/admin.py
+++ hakureiclub_app/core_view/admin.py
@@ -11,75 +11,74 @@ acti = ActiInfo()
 ainfo = AuthInfo() 
 admincp = Blueprint('admincp')
 
-async def login_manger(request):
-    the_cookie = request.cookies.get('github_login')
-    the_token = request.cookies.get('github_token')
-    if not the_cookie == None or not the_token == None :
-        code = await getuser.getorg(the_cookie,the_token)
-        if not code == 200 : 
-            return text('你沒有權限噢')
-    else:
-        res = html('Redirect',status = 302)
-        res.headers['Location'] = authit.getGitHubAuth()
-        return res 
+
+def lm_require(func):
+    async def wrapper(request):
+        the_cookie = request.cookies.get('github_login')
+        the_token = request.cookies.get('github_token')
+        if not the_cookie == None or not the_token == None :
+            code = await getuser.getorg(the_cookie,the_token)
+            if not code == 200 : 
+                return text('你沒有權限噢')
+            else:
+                return func(request)
+        else:
+            res = html('Redirect',status = 302)
+            res.headers['Location'] = authit.getGitHubAuth()
+            return res 
+    return wrapper
 
 
+@lm_require
 @admincp.route('/')
 async def adminindex(request):
-    if await login_manger(request) == None:
-        allev = acti.all()
-        return render_template('admin/index.html',**locals())
-    return await login_manger(request)
+    allev = acti.all()        
+    return render_template('admin/index.html',**locals())
 
 
+@lm_require
 @admincp.route('/editor')
 async def admineditor(request):
-    if await login_manger(request) == None:
-        blogtitle = 'nopost'
-        blogpost = blog.all()
-        return render_template('admin/editor.html',**locals())
-    return await login_manger(request)
+    blogtitle = 'nopost'
+    blogpost = blog.all()
+    return render_template('admin/editor.html',**locals())
 
 
+@lm_require
 @admincp.route('/edit/<posted>')
 async def adminedit(request,posted):
-    if await login_manger(request) == None:
-        blogpost = blog.all()
-        blogcontent = blog.find(posted)['markdown']
-        blogtitle = blog.find(posted)['title']
-        return render_template('admin/editor.html',**locals())
-    return await login_manger(request)
+    blogpost = blog.all()
+    blogcontent = blog.find(posted)['markdown']
+    blogtitle = blog.find(posted)['title']
+    return render_template('admin/editor.html',**locals())
 
 
+@lm_require
 @admincp.route('/delev/<name>')
 async def delev(request,name):
-    if await login_manger(request) == None:
-        acti.remove(name)
+    acti.remove(name)
+    res = html('Redirect',status = 302)
+    res.headers['Location'] = '/admin/'
+    return res 
+    
+
+@lm_require
+@admincp.route('/new-post')
+async def adminindex(request):
+    if request.method == 'POST' :
+        title = request.form.get('title')
+        blog.init(title,str(request.form.get('markdown')))
+        return text('OK')
+
+@lm_require
+@admincp.route('/add_event')
+async def add_event(request):
+    if request.method == 'POST' :
+        name = request.form.get('name')
+        time = request.form.get('time')
+        place = request.form.get('place')
+        acti.init(name,time,place)
         res = html('Redirect',status = 302)
         res.headers['Location'] = '/admin/'
         return res 
-    return await login_manger(request)
-    
-
-@admincp.route('/new-post')
-async def adminindex(request):
-    if await login_manger(request) == None:
-        if request.method == 'POST' :
-            title = request.form.get('title')
-            blog.init(title,str(request.form.get('markdown')))
-            return text('OK')
-    return await login_manger(request)
-
-@admincp.route('/add_event')
-async def add_event(request):
-    if await login_manger(request) == None:
-        if request.method == 'POST' :
-            name = request.form.get('name')
-            time = request.form.get('time')
-            place = request.form.get('place')
-            acti.init(name,time,place)
-            res = html('Redirect',status = 302)
-            res.headers['Location'] = '/admin/'
-            return res 
-    return await login_manger(request)
 
